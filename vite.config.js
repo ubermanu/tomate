d7e993ca4f46fd2343fc00b82417bcde08badb33
ubermanu <e.vodor@gmail.com>
2020-12-06 13:37:44 +0100

    Use pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-// @ts-check
 import reactPlugin from 'vite-plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './src/manifest.json';
 
 /**
  * @type { import('vite').UserConfig }
  */
 const config = {
   jsx: 'react',
-  plugins: [reactPlugin],
+  plugins: [reactPlugin, VitePWA({ manifest })],
 };
 
 export default config;
