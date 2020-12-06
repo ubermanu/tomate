dbfa1fd69a934073dc6e1160f9f7051d7ba24bdf
ubermanu <e.vodor@gmail.com>
2020-12-06 12:25:23 +0100

    Install vite and setup src

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import reactPlugin from 'vite-plugin-react'
+
+/**
+ * @type { import('vite').UserConfig }
+ */
+const config = {
+  jsx: 'react',
+  plugins: [reactPlugin],
+  optimizeDeps: {
+    exclude: ['prop-types']
+  }
+}
+
+export default config
